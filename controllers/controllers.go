0db7db0e78f457a13bde9ba968e123142f2bb5d2
Kamogelo Sekhukhune <kamogelosekhukhune7@gmail.com>
2023-12-20 15:42:56 +0200

    full signup function,vendoring bug

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/kamogelosekhukhune777/ecom-cart/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func HashPassword(password string) string {
@@ -37,13 +38,56 @@ func SignUP() gin.HandlerFunc {
 			return
 		}
 
+		//check if user exists in database.
 		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
 		if err != nil {
 			log.Panic(err)
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
 			return
 		}
-		//01:37
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "user already exists"})
+		}
+
+		count, err := userCollection.CountDocuments(ctx, bson.M{""})
+
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "this phone is already in use"})
+			return
+		}
+
+		password := HashPassword(user.Password)
+		user.Password = &password
+
+		user.CreatedAt, _ = time.Parse(time.RFC3399, time.Now().Format(time.RFC3399))
+		user.UpadatedAT, _ = time.Parse(time.RFC3399, time.Now().Format(time.RFC3399))
+		user.ID = primitive.NewObjectID()
+		user.User_ID = user.ID.Hex()
+
+		token, refreshToken, _ := generate.TokenGenerator(*user.Email, *user.First_Name, *user.Last_Name, *user.User_ID)
+		user.Token = &refreshToken
+		user.Refresh_Token = &refreshToken
+
+		user.Cart = make([]models.ProductUser, 0)
+		user.Address_Details = make([]models.Address, 0)
+		user.Order_status = make([]models.Order, 0)
+
+		_, inserterr := userCollection.InsertOne(ctx, user)
+		if inserterr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "the user did not get created"})
+			return
+		}
+		defer cancel()
+
+		c.JSON(http.StatusCreated, "successfully signed in!")
 	}
 }
 
